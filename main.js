7ff74ef797649b6823db45e57422e2912fb13f96
jordanchan168 <jchan16@stuy.edu>
2017-03-06 21:36:14 -0500

    grow circle

--- main.js
+++ main.js
@@ -1,16 +1,67 @@
 var pic = document.getElementById("vimage");
 var b = document.getElementById("b");
 
+var clear = function() {
+    while (pic.lastChild) {
+        pic.removeChild(pic.lastChild);
+    }
+}
+
+var mkCircle = function() {
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", 250);
+    c.setAttribute("cy", 250);
+    c.setAttribute("r", "0");
+    c.setAttribute("fill", "red");
+    c.setAttribute("stroke", "black");
+    return c;
+};
+
+var intervalID
+
+var animate = function() {
+    stop();
+    clear();
+    var c = mkCircle();
+    pic.appendChild(c);
+    var enlarge = true;
+    var radius = 0;
+    var drawDot = function() {
+        c.setAttribute("r", radius.toString());
+        pic.appendChild(c);
+        if (radius == 0) {
+            enlarge = true;
+        }
+        if (radius == 250) {
+            enlarge = false;
+        }
+        if (enlarge) {
+            radius += 1;
+        }
+        else {
+            radius -= 1;
+        }
+    };
+    intervalID = window.setInterval(drawDot, 16);
+};
+
+pic.addEventListener("click", animate);
+
+var stop = function() {
+    window.clearInterval(intervalID);
+}
+
+b.addEventListener("click", stop);
+
+
+//=============================================
 firstDot = true;
 prevX = "0";
 prevY = "0";
-
 var draw = function() {
-
     // get x, y cor
     var x = (event.clientX - 10).toString();
     var y = (event.clientY - 10).toString();
-
     // draw line
     if (!firstDot) {
         var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
@@ -20,7 +71,6 @@ var draw = function() {
         l.setAttribute("y2", y);
         l.setAttribute("stroke", "black");
         pic.appendChild(l);
-
     // redraw prev circle
         var c2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         c2.setAttribute("cx", prevX);
@@ -30,7 +80,6 @@ var draw = function() {
         c2.setAttribute("stroke", "black");
         pic.appendChild(c2);
     }
-
     // draw circle
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c.setAttribute("cx", x);
@@ -39,21 +88,9 @@ var draw = function() {
     c.setAttribute("fill", "red");
     c.setAttribute("stroke", "black");
     pic.appendChild(c);
-
     // update vars
     firstDot = false;
     prevX = x;
     prevY = y;
 }
-
-pic.addEventListener("click", draw);
-
-
-var clear = function() {
-    while (pic.lastChild) {
-        pic.removeChild(pic.lastChild);
-    }
-    firstDot = true;
-}
-
-b.addEventListener("click", clear);
+//pic.addEventListener("click", draw);
